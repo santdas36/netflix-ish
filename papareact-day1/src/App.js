1ea502a71ce624ab0d3360b370e747acaf85433c
santdas36 <santdas36@gmail.com>
2021-02-03 10:53:49 +0530

    Initial commit

---
+++ papareact-day1/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+import Portfolio from "./Components/Portfolio";
+
+import "./App.css";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;
