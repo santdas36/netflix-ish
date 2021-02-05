2bc511e6e8259740446e2356ba996b7703064027
santdas36 <santdas36@gmail.com>
2021-02-05 20:42:44 +0530

    checkout

--- papareact-day4/src/Profile.js
+++ papareact-day4/src/Profile.js
@@ -4,10 +4,29 @@ import userIcon from './assets/nfuser.jpg';
 import {auth} from './firebase';
 import {loadStripe} from '@stripe/stripe-js';
 
-const stripe = loadStripe(process.env.STRIPE_PK);
-console.log(process.env.STRIPE_PK, process.env.STRIPE_SK);
+const stripe = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(process.env.STRIPE_PK);
 
 function Profile({user}) {
+		
+	const checkout = (priceId) => {
+		fetch(`/api/create-checkout-session?priceId=${priceId}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+		}).then((result) => result.json()).then((data) => {
+			if (data.error) {
+				console.log(data.error.message)
+			} else {	
+				stripe.redirectToCheckout({
+					sessionId: data.sessionId,
+					customerEmail: user.email,
+				})
+			}
+		});
+	}
+
 	
   return (
     <div className="profile">
@@ -25,7 +44,7 @@ function Profile({user}) {
 		<li>480p SD Streaming</li>
 		</ul>
 		<p className="price">$ 19.99<small>/month</small></p>
-		<button>Activate Now</button>
+		<button onClick={()=> checkout('price_1IHThZGVr4f6jXHSzK1GUgJd')}>Activate Now</button>
 	</div>
 	<div className="pack">
 		<h3>Netflix Lite</h3>
@@ -34,7 +53,7 @@ function Profile({user}) {
 		<li>480p SD Streaming</li>
 		</ul>
 		<p className="price">$ 29.99<small>/month</small></p>
-		<button>Activate Now</button>
+		<button onClick={()=> checkout('price_1IHTmPGVr4f6jXHSav4jhyQg')}>Activate Now</button>
 	</div>
 	<div className="pack active">
 		<h3>Netflix HD</h3>
@@ -43,7 +62,7 @@ function Profile({user}) {
 		<li>1080p HD Streaming</li>
 		</ul>
 		<p className="price">$ 39.99<small>/month</small></p>
-		<button>Renews in 12 days</button>
+		<button onClick={()=> checkout('price_1IHTs5GVr4f6jXHSID3JNFyr')}>Renews in 12 days</button>
 	</div>
 	<div className="pack">
 		<h3>Netflix Premium</h3>
@@ -52,7 +71,7 @@ function Profile({user}) {
 		<li>4K UHD Streaming</li>
 		</ul>
 		<p className="price">$ 499.99<small>/year</small></p>
-		<button>Activate Now</button>
+		<button onClick={()=> checkout('price_1IHTucGVr4f6jXHSbmX7mkFI')}>Activate Now</button>
 	</div>
 </div>
 <p className="pack__info">All plans come with a 30 days FREE trial period. Cancel anytime.</p>
