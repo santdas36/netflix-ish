f289bf4e75b43ea273de9e85dcbaf39a3e19d970
santdas36 <santdas36@gmail.com>
2021-02-05 22:23:33 +0530

    location

--- papareact-day4/src/Success.js
+++ papareact-day4/src/Success.js
@@ -6,10 +6,11 @@ import {useLocation} from 'react-router-dom';
 function Success({setLoading}) {
 	
 	const stripe = useStripe();
+	const location = useLocation();
 	
 	useEffect(()=> {
 		setLoading(true);
-		const sessionId = new URLSearchParams(useLocation().search).get('session_id');
+		const sessionId = new URLSearchParams(location.search).get('session_id');
 		fetch(`/retrieve-session?sessionId=${sessionId}`).then((response)=>response.json()).then((data)=> {console.log(data);setLoading(false);}).catch((error)=> console.log(error.message));
 	}, []);
 	
