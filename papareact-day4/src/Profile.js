7cb39333ee0941edc9bec30086525c3f9cbd9414
santdas36 <santdas36@gmail.com>
2021-02-06 00:53:18 +0530

    renewal

--- papareact-day4/src/Profile.js
+++ papareact-day4/src/Profile.js
@@ -71,7 +71,7 @@ function Profile({user, setLoading}) {
 <h1>Hi, {user?.displayName}!</h1>
 		<button className="signout" onClick={()=> auth.signOut()}>Sign Out</button>
 </div>
-<h3>Manage your subscription</h3>
+<h3>{activePack ? 'Manage your subscription' : 'Subscribe Now to Start Watching'}</h3>
 <div className="packs">
 {packs.map((pack) => (
 	<div className={pack.priceId === activePack ? 'pack active' : 'pack'}>
@@ -81,7 +81,7 @@ function Profile({user, setLoading}) {
 		<li>{pack.d2}</li>
 		</ul>
 		<p className="price">$ {pack.price}<small>/{pack.duration}</small></p>
-		<button disabled={pack.priceId === activePack} onClick={(e)=> checkout(pack.priceId)}>{pack.priceId === activePack ? 'Renews in' : 'Activate Now'}</button>
+		<button disabled={pack.priceId === activePack} onClick={(e)=> checkout(pack.priceId)}>{pack.priceId === activePack ? `Renews in ${parseInt((subs.current_period_end - subs.current_period_end)/(24*60*60))}` : 'Activate Now'}</button>
 	</div>
 ))}
 </div>
